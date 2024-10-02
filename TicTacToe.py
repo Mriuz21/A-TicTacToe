05b12a986bf3adb54bfa7922b1afbce17e13aa6f
Marius Dudui <marius.dudui03@e-uvt.ro>
2024-10-02 16:29:30 +0300

    Initiazile the tictactoe board

---
+++ TicTacToe.py
@@ -0,0 +1,12 @@
+class TicTacToe :
+    def __init__(self,size):
+        #This constructor is used to initialize a size*size board
+        self.size = size
+        self.board = [None] *(size*size)
+        self.currentPlayer = 'X'
+    def print_board(self):
+        pass
+        
+
+        
+        
