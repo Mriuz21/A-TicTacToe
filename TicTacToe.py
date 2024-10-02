5c1864c9f1215cfb3655783317cb1df1d0c0c9a8
Marius Dudui <marius.dudui03@e-uvt.ro>
2024-10-02 16:41:51 +0300

    print_board method is up

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,8 +5,18 @@ class TicTacToe :
         self.board = [None] *(size*size)
         self.currentPlayer = 'X'
     def print_board(self):
-        pass
-        
+        for i in range(self.size):
+            rows = []
+            for j in range(self.size):
+                index = i*self.size+j
+                if self.board[index] is not None:
+                    rows.append(self.board[index])
+                else:
+                    rows.append(" ")
+            print("|".join(rows))
+            if i < self.size -1:
+                print("-" * (self.size * 2 - 1))  # Print a separator line
 
-        
-        
+
+game = TicTacToe(5)
+game.print_board()
